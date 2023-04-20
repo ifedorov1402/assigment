98e1ce80b19ad861578b660af367275fea84e418
Ivan Fedorov <fedorov.ivan@hotmail.com>
2023-04-19 21:33:45 -0400

    daily-planet-editor

--- exersises/daily-planet-editor/paper.js
+++ exersises/daily-planet-editor/paper.js
@@ -11,31 +11,25 @@ just fix the existing ones.
 
 var enemies = ["Lex", "Batman", "Darkseid", "Brainiac", "General Zod", "Doomsday"];
 
-function whoWins(isThereKryptonite, enemyName)
-{
-if (!isThereKryptonite)
-{
-    return "Superman beats " +enemyName+ ", of course";
-}
-
-else{
-    return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyName +" could possibly win this one.";
-}
+function whoWins(isThereKryptonite, enemyName) {
+    if (!isThereKryptonite){
+        return "Superman beats " + enemyName + ", of course";
+    } else {
+        return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyName +" could possibly win this one.";
     }
+}
 
-for (var i = 0; i < enemies.length; i++)
-{
+for (var i = 0; i < enemies.length; i++) {
     var isThereKryptonite;
-    if (i%2=== 0) {
+    if (i % 2 === 0) {
         isThereKryptonite = true;
     } else {
         isThereKryptonite = false;
     }
-    console.log(whoWins(isThereKryptonite,enemies[i]));
+    console.log(whoWins(isThereKryptonite, enemies[i]));
 }
 
-function howAttractedIsLoisLaneToMe ()
-{
+function howAttractedIsLoisLaneToMe () {
     // 1 is not at all attracted, 10 is "super" attracted...
     return Math.floor((Mat.random()*10)+1);
 }
@@ -45,10 +39,12 @@ consol.log(howAttractedIsLoisLaneToMe());
 var clarkKent = true;
 var superman = false;
 
-while (clarkKent){
+while (clarkKent) {
     console.log("I'm just a nerdy columnist");
-    var phoneBoothQuickChange =Math.random();
-    if (phoneBoothQuickChange>= 0.5) {
-        clarkKent=false;
-        superman=true;
-        console.log("Now I'm Superman!");}}
+    var phoneBoothQuickChange = Math.random();
+    if (phoneBoothQuickChange >= 0.5) {
+        clarkKent = false;
+        superman = true;
+        console.log("Now I'm Superman!");
+    }
+ }
